b1e0a1c2cbcb0a1c5ed8489919a2fa39293468c6
Riddhi Dhawan <riddhidhawan4@gmail.com>
2026-04-14 11:57:20 +0530

    fixed dev errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default {
-  // other configurations ...
-  plugins: [react()].filter(Boolean),
-  // other configurations ...
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 }
